05a51bb4f6671017827e5faec692b1f277408309
drambol <cf88ex@gmail.com>
2016-06-08 13:54:43 +0800

    Test Jenkins

--- Jenkins.bat
+++ Jenkins.bat
@@ -3,5 +3,5 @@
 cd C:\Softwares\Jenkins
 start java -jar jenkins.war --httpPort=8084
 
-ping 127.0.0.1 -n 15 > nul
-taskkill /F /IM conhost.exe
+::ping 127.0.0.1 -n 15 > nul
+::taskkill /F /IM conhost.exe
